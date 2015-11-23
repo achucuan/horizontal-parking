f4352afd1014e72763763281b12ebee91ca85673
Jorge <jorgetp86@gmail.com>
2015-11-22 21:03:15 -0600

    agregando SlotController

---
+++ project/app/Http/Controllers/SlotController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class SlotController extends BaseController
+{
+	public function buildDirtySlot()
+	{
+
+	}	
+
+
+	public function buildPatternSlot()
+	{
+		
+	}   
+}
