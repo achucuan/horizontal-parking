d24b40063ff1cab6735b17be6f24932c6801a30a
Alberto Chucuan <cachlomeli@gmail.com>
2015-12-02 17:52:25 -0600

    added links

--- project/resources/views/nav/main-nav.blade.php
+++ project/resources/views/nav/main-nav.blade.php
@@ -6,6 +6,6 @@
     <dd><a href="/patterns/facade">Facade</a> - Alberto Chucuan</dd>
     <dd><a href="/patterns/create-slot">Decorator</a> - Alberto Chucuan</dd>
     <dt>Behavioral</dt>
-    <dd>Observer - Jorge Tiznado</dd>
-    <dd>Strategy - Jorge Tiznado</dd>
+    <dd><a href="/patterns/observer">Observer</a> - Jorge Tiznado</dd>
+    <dd><a href="/patterns/make-html">Strategy</a> - Jorge Tiznado</dd>
 </dl>
